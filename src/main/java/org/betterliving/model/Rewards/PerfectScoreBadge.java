dfb8c28cebeb45827cff374265379bae7da71d04
bananasabah <jordanwalter900@gmail.com>
2026-06-04 13:07:32 +0800

    Created Reddemable interface and a one class file

---
+++ src/main/java/org/betterliving/model/Rewards/PerfectScoreBadge.java
@@ -0,0 +1,13 @@
+package org.betterliving.model.Rewards;
+
+public class PerfectScoreBAdge implements Rewardable{
+    private final String badgeName = "Perfect Score Badge";
+    private final int requiredScore;
+
+    public PerfectScoreBAdge(int requiredScore) {
+        this.requiredScore = requiredScore;
+    }
+
+    @Override
+    public boolean 
+}
